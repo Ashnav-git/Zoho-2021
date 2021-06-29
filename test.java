a3cbbdd1c475a1b6ca4c221a741b5eea49e506e0
Ashnav-git <69746930+Ashnav-git@users.noreply.github.com>
2021-06-29 10:57:46 +0530

    Test Done

---
+++ test.java
@@ -0,0 +1,47 @@
+package exam;
+import java.util.*;
+public class test {
+
+	
+	public static void printpattern(String s,int n)
+	{
+		char a[]=s.toCharArray();
+		int midIndex = n/2;
+	    for(int i=0;i<n;i++)
+		{
+	       int loop=i;
+			for(int k=0;k<n-i-1;k++)
+			{
+				System.out.print(" ");
+			}
+			if(midIndex+i > n || midIndex+i == n  )
+			{
+				loop=midIndex;
+			}
+			for(int j=midIndex;j<=midIndex+loop;j++)
+			{
+				System.out.print(a[j]);
+			}	
+			if(midIndex+i > n || midIndex+i == n  )
+			{
+			for(int j=0;j<i-midIndex;j++)
+			{
+				System.out.print(a[j]);
+			}
+			}
+			System.out.println();
+		}
+	    
+	}
+		
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+        int n = 7;
+		String s="WELCOME";
+		
+		printpattern(s,n);
+	}
+
+}
